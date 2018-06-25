51dc18ae1c5b4c2c83c1e9fab1b2ad6a185df012
Kevin Lu <kevin@smartzeta.com>
2018-06-25 16:52:22 +1200

    Remove unused files

--- UnityProject/Assets/WebPlayerTemplates/LayaDemo/LayaAir3DSample.js
+++ UnityProject/Assets/WebPlayerTemplates/LayaDemo/LayaAir3DSample.js
@@ -2,5 +2,4 @@ Laya3D.init(0, 0, true);
 Laya.stage.scaleMode = Laya.Stage.SCALE_FULL;
 Laya.stage.screenMode = Laya.Stage.SCREEN_NONE;
 Laya.Stat.show();
-var scene = Laya.stage.addChild(new Laya.Scene());
-var sprite3D = scene.addChild(Laya.Sprite3D.load('res/football/ball.lh'));
+var scene = Laya.stage.addChild(Laya.Scene.load('res/pitch/pitch.ls'));
