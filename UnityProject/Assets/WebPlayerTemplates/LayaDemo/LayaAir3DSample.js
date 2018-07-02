d949b7b314c94c3052f696a7b66e7d684ff92a38
Kevin Lu <kevin@smartzeta.com>
2018-07-02 13:20:40 +1200

    Add new assets

--- UnityProject/Assets/WebPlayerTemplates/LayaDemo/LayaAir3DSample.js
+++ UnityProject/Assets/WebPlayerTemplates/LayaDemo/LayaAir3DSample.js
@@ -2,4 +2,4 @@ Laya3D.init(0, 0, true);
 Laya.stage.scaleMode = Laya.Stage.SCALE_FULL;
 Laya.stage.screenMode = Laya.Stage.SCREEN_NONE;
 Laya.Stat.show();
-var scene = Laya.stage.addChild(Laya.Scene.load('res/pitch/pitch.ls'));
+var scene = Laya.stage.addChild(Laya.Scene.load('res/footballScene/pitch.ls'));
